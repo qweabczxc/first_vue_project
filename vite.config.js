5834b45c46a5c4f19f2fa338a680759f0d4619bb
qweabczxc <molochko544@gmail.com>
2024-04-29 14:56:44 +0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  base: '/first_vue_project/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
